42c603ca66bae6f89e64b454662e2643a2d770fb
Matheo Quiroga <matheo.quiroga@ucb.edu.bo>
2025-08-21 08:51:51 -0400

    feat: saludador.js -> agregar saludar por nombre y refactor: saludador

--- src/presenter.js
+++ src/presenter.js
@@ -1,11 +1,13 @@
 //import sumar from "./sumador";
 
-import saludarConHola from "./saludador";
+import { SaludarPorNombre, SaludarConHola, saludarConHola } from "./saludador.js";
 
+const nameInput = document.querySelector("#name-input");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  div.innerHTML = "<h3>" + saludarConHola() + "</h3>";
+  const saludador = nameInput.value==""? new SaludarConHola() : new SaludarPorNombre(nameInput.value);
+  div.innerHTML = "<h3>" + saludador.saludar() + "</h3>";
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,5 +1,34 @@
+class Saludador{
+    message;
+    constructor(){
+        this.message= " Bienvenido!";
+    }
+    saludar(){
+        return this.message;
+    }
+}
+
+class SaludarConHola extends Saludador{
+    constructor(){
+        super();
+    }
+    saludar(){
+        return  "Hola, " + super.saludar();
+    }
+}
+class SaludarPorNombre extends Saludador{
+    nombre;
+    constructor(nombre){
+        super();
+        this.nombre= nombre;
+    }
+    saludar(){
+        return "Hola, " + this.nombre + super.saludar();
+    }
+}
+
 function saludarConHola(){
     return "Hola, Bienvenido!";
 }
 
-export default saludarConHola;
+export { SaludarPorNombre, SaludarConHola, saludarConHola, Saludador };
